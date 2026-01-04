db45c4102c87f43db6a8f1c2ecbac008b6717ed5
Ronald Guido <rjdguido@gmail.com>
2026-01-04 20:43:44 +0000

    Move Firebase web config to env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBRW5Oa0V6VVPQHV5cVdE2JrZi93HKPs_M",
-  authDomain: "argo-4cc6b.firebaseapp.com",
-  projectId: "argo-4cc6b",
-  storageBucket: "argo-4cc6b.firebasestorage.app",
-  messagingSenderId: "798062548748",
-  appId: "1:798062548748:web:0a6bea808aaaa93b529b29",
-  measurementId: "G-F2D4G46E5G",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export const firebaseApp = getApps().length
